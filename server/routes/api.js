4fff04717c23caad65b15c9139fd7691d6f71e14
Mitchell <mitchell.rian.smith@gmail.com>
2020-08-22 09:44:43 -0700

    Remove test Items insert, and add get items route

--- server/routes/api.js
+++ server/routes/api.js
@@ -10,15 +10,18 @@ mongoose.connect('mongodb://localhost/ecomm', {useNewUrlParser: true,  useUnifie
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    Items.create({
-        sku: "test",
-        name: "Test",
-        desc: "Test", 
-        price: 1.00,
-        inv_level: 5,
-        tax: 0.02 // TODO add a functionality to have different tax codes with different values
-    })
     res.send('respond with an api resource');
 });
+
+router.get('/items', function(req, res, next){
+    Items.find(req.query)
+	.then(parts => {
+		res.json({data: parts});
+	})
+	.catch(error => {
+		res.json({status: "error", data: error});
+	});
+});
+
 
 module.exports = router;
