61ed89a918bb675b7125fe557681789f0ca03ac5
Mitchell <mitchell.rian.smith@gmail.com>
2020-08-22 09:26:53 -0700

    Add items model to api route

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+//Models
+var Items = require('../models/item');
+
+//Database
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/ecomm', {useNewUrlParser: true,  useUnifiedTopology: true });
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    Items.create({
+        sku: "test",
+        name: "Test",
+        desc: "Test", 
+        price: 1.00,
+        inv_level: 5,
+        tax: 0.02 // TODO add a functionality to have different tax codes with different values
+    })
     res.send('respond with an api resource');
 });
 
