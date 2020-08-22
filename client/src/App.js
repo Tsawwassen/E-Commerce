13161f6434a796aebbffbe8d7ca6eef099563a56
Mitchell <mitchell.rian.smith@gmail.com>
2020-08-22 10:48:11 -0700

    Add home, items, and orders routes to project

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,34 @@
+//Imports
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navigation from './components/nav.js';
+
+//CSS
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    
+    <Router>
+      <div className="app">
+        <Navigation />
+        <div className="container">
+          <Switch>
+            <Route path="/items">
+              <h1>Items</h1>
+            </Route>
+            <Route path="/orders">
+              <h1>Orders</h1>
+            </Route>
+            <Route path="/">
+              <h1>home</h1>
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+    
   );
 }
 
