d17ec0dcf528bdc52800ac1f547a0ea4b63a12b1
Mitchell <mitchell.rian.smith@gmail.com>
2020-08-23 08:03:19 -0700

    Add logic to update cart when items are added

--- client/src/App.js
+++ client/src/App.js
@@ -13,12 +13,50 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
+      cart:[],
     
-
     }
+
+    this.handleAddToCart = this.handleAddToCart.bind(this);
   }
 
-  
+  handleAddToCart(event){
+    //console.log(`Dropdown Value ${event.target[0].value}\n SKU Name ${event.target[0].name}`);
+    //console.table(JSON.parse(event.target[0].name)); //Access to the full item object
+    //console.log(event.target[0].value); //Access to the dropdown list value
+
+    let selected_item = JSON.parse(event.target[0].name)
+    let event_item = {
+      sku: selected_item.sku,
+      price: Number(selected_item.price),
+      qty: Number(event.target[0].value)
+    }
+    
+    let t_cart = this.state.cart;
+    let added_flag = false; //boolean flag to determine if the event_item is in the cart already or not
+
+    //Check if the event_item is in the cart.
+    //  If it is, update the qty on cart.
+    //  Else, add the event_item to cart.
+    t_cart.some((item, index) => {
+        if(item.sku === event_item.sku){
+          item.qty += event_item.qty;
+          added_flag = true;
+          return true;
+        }
+        return false;
+    });
+
+    if(!added_flag){
+      t_cart.push(event_item);
+    }
+    
+    this.setState({cart: t_cart});
+    event.preventDefault();
+  }
+
+
+
   render() {
     return (
       <Router>
@@ -27,7 +65,7 @@ class App extends Component {
           <div className="container">
             <Switch>
               <Route path="/items">
-                <Items />
+                <Items onSubmit={this.handleAddToCart} />
               </Route>
               <Route path="/orders">
                 <h1>Orders</h1>
