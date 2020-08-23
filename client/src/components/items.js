93379e4fb386d00d1038f105bba954331fa880d9
Mitchell <mitchell.rian.smith@gmail.com>
2020-08-23 08:04:24 -0700

    Add logic to send selected item to parent component when added to cart

--- client/src/components/items.js
+++ client/src/components/items.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import { Button } from 'react-bootstrap';
+import {Form, Button, Container, Row, Col } from 'react-bootstrap';
 
 //SCSS
 import './items.scss';
 
+//I question if this componenet should even have a state values
+//// In my head, I expected this componenet to only have JSX functions, but I was having trouble finding where the /api/items fetch call should be called to populate hte screen
 class Items extends Component {
   constructor(props){
     super(props);
     this.state ={
       itemList: [],
     }
-
-    
-    
     this.displayItem = this.displayItem.bind(this);
   }
 
@@ -24,26 +23,44 @@ class Items extends Component {
     });
   }
 
+  //TODO : I would like to clean up this code and improve how I handle div layouts
   displayItem(item){
-  
-    return (<div className="itemBlock" key={item._id}>
+    return (
+            <div className="itemBlock" key={item._id}>
               <div className="image"></div>
               <div><h2 className="name">{item.name}</h2></div>
               <div className="desc"><p>{item.desc}</p></div>
               <div> <p className="price">${item.price} each</p> <p className="inventory"> Available : {item.inv_level}</p></div>
-              <Button variant="primary" type="submit" > Add to Cart </Button>
+              <div>
+              <Container>
+                <Form onSubmit={this.props.onSubmit}>
+                  <Row>
+                    <Col md={1.2}>
+                    <Form.Control as="select" custom className="col-sm" name={JSON.stringify(item)}>
+                      <option>1</option>
+                      <option>2</option>
+                      <option>3</option>
+                      <option>4</option>
+                      <option>5</option>
+                    </Form.Control>
+                    </Col>
+                    <Col>
+                    <Button variant="primary" type="submit"> Add to Cart </Button>
+                    </Col>
+                  </Row>
+                </Form>
+              </Container>
+              </div>
               <hr />
             </div>
           );
   }
 
   render () {
- 
     return (
       <div className="items">
       <h1>Please select items to add you cart</h1>
         {this.state.itemList.map( (item) => this.displayItem(item) )}
-      
       </div>
     );
   }
