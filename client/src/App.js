714786dd33084494956ac3832dd7a66fce2238e2
Mitchell <mitchell.rian.smith@gmail.com>
2020-08-23 11:49:51 -0700

    Move navigation componenet to rnder inside ecomm components

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 //Imports
 import React from 'react';
-import Navigation from './components/nav.js';
 import EComm from './components/ecomm.js';
 
 //CSS
@@ -10,10 +9,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
     return (
         <div className="app">
-          <Navigation />
-          <div className="container">
             <EComm />
-          </div>
         </div>
     );
 }
