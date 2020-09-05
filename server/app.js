b885045df2d44013f9a9bb29b0637fb7a1aea249
Mitchell <mitchell.rian.smith@gmail.com>
2020-09-05 07:11:06 -0700

    Receive order from frontend

--- server/app.js
+++ server/app.js
@@ -8,6 +8,13 @@ var apiRouter = require('./routes/api');
 
 var app = express();
 
+// require bodyParser
+var bodyParser = require('body-parser');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
 
 app.use(logger('dev'));
 app.use(express.json());
